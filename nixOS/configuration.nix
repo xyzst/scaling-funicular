7a9727eef8e596ad5a7d224a3d6c12a720d7cbb0
Darren Rambaud <xyzst@users.noreply.github.com>
2020-05-30 02:01:15 -0500

    nixos: add afp service
    - add afp server with netatalk, avahi

--- nixOS/configuration.nix
+++ nixOS/configuration.nix
@@ -24,6 +24,28 @@
   };
 
   services = {
+    avahi = {
+      enable = true;
+      publish = {
+        enable = true;
+        userServices = true;
+      };
+    };
+    netatalk = {
+      enable = true;
+      extraConfig = ''
+        mimic model = TimeCapsule6,106
+        log level = default:warn
+        log file = /var/log/afpd.log
+        hosts allow = 192.168.1.0/24
+        set password = yes
+      [ab aeterno]
+        path = /tmbak
+        valid users = timemachine
+        time machine = yes
+        vol size limit = 750000
+      '';
+    };
     sshd = {
       enable = true;
     };
@@ -32,6 +54,12 @@
   networking = {
     hostName = "rambaud";
     enableIPv6 = true;
+    firewall = {
+      allowedTCPPorts = [
+        548
+        636
+      ];
+    };
   };
 
   fonts = {
@@ -85,6 +113,20 @@
     hardwareClockInLocalTime = false;
   };
 
+  systemd = {
+    services = {
+      timeMachineUserSetup = {
+        description = "Idempotent directory setup for timemachine user";
+        requiredBy = [
+          "netatalk.service"
+        ];
+        script = '' mkdir -p /tmbak
+                    chown timemachine:users /tmbak
+                    chmod 0750 /tmbak '';
+      };
+    };
+  };
+
   users = {
     enforceIdUniqueness = true;
     mutableUsers = true;
@@ -101,7 +143,11 @@
         shell = pkgs.zsh;
         openssh.authorizedKeys.keys = [
           "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC1tyVJGEVwUniarTymeAKm1q3Sr6JJXG0UPzUKzhW6zBdkGfPWFmm9SXLomBrUWYbSjEFtwoZ32GfMQnVcKiyfYWNyXzesCc1JP1/33ay0I9SnGTv2W9llkz5ur/zuqHSX4dAiyUsVc1XmSOo7k9+AdAqT/TYY/TZL/5jYsqIYF6RL3KITm49kMCCJkOqODAqUTBosgah0XSlkU9mlk8wZpsgPWmffNbMdyu9WN5p64AR+75u8CACzLAZsw6HM5lf1KcpH0sgRPUImAkqW8ywx6pROcwwVGHY5uBxehozexl1UPpSMTYTlA1MomVIL0cWvwgSQ+lg59ZG7VGJqRdj7dK0uZn/wJ3lH4jiA3L5I1viv7fpTaEZdWq9Rj1wyIB2EhMoiA33wJooYgIyeBhldGsKFEXz2DLCdOZl9BIGUak7oa7bD0Q7qsoaijmaqjuM+oSraGl7zcbVELyr0o+bpDz5evD7jWxtvLq45TEPezAxo0wYUF+byLzHXVviAAVHh/yQvXi+GNYO6Ut65oZt+VWog4vh5UEogudv93csByPPbGftRdyT+W6g7wyhfq3EngwudqyOMnbANDI6LHbN2dIqT8zEwjUz6YJAjlqHR3ZpYwzs+jfmnUSc8ODI1uoOpXb1vTqHsMzWhsejfbVB9LAUuAbPJsyCnU1zLhrqCGQ== xyzst@users.noreply.github.com"
-        ]; 
+        ];
+      };
+      timemachine = {
+        initialHashedPassword = "$6$4CanI/KMP$j6jb8n1njLqqVPzsi5OgJgKa/L62UHSUr/o4.Q6qEXrJq7N3fFyi36kOAn5kG6cnlyvv.bsG7Kug/gaV8j2Lx.";
+        description = "Time machine use only";
       };
     };
     motd = 
